3b7e9de00558f61138bb27bfbd40fc63b73330c7
Moonkuro101 <songpontonku@gmail.com>
2024-08-18 00:04:42 +0700

    change smoothindication

--- lib/screens/intro_screen/intro_screen.dart
+++ lib/screens/intro_screen/intro_screen.dart
@@ -1,8 +1,8 @@
 import 'package:fish_finder/screens/intro_screen/screen1.dart';
 import 'package:fish_finder/screens/intro_screen/screen2.dart';
 import 'package:fish_finder/screens/intro_screen/screen3.dart';
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class IntroScreen extends StatefulWidget {
@@ -13,47 +13,102 @@ class IntroScreen extends StatefulWidget {
 
 class _IntroScreenState extends State<IntroScreen> {
   PageController pageController = PageController();
+  String buttonText = 'Next'; // Initialize with 'Next'
+  bool isLastPage = false;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xfff7eedd),
-      body: Stack(
-        children: [
-          PageView(
-            controller: pageController,
-            children: const [
-              Screen1(),
-              Screen2(),
-              Screen3(),
-            ],
-          ),
-          Container(
-            alignment: const Alignment(0, 0.9),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SmoothPageIndicator(
-                  effect: const ExpandingDotsEffect(
-                    activeDotColor: Color(0xffF69461),
-                    dotColor: Color(0xff3652AD)
-                  ),
-                  controller: pageController,
-                  count: 3,
-                  onDotClicked: (index) {
-                    pageController.animateToPage(
-                      index,
-                      duration: const Duration(milliseconds: 500),
-                      curve: Curves.easeInOut,
-                    );
-                  },
+ @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: const Color(0xfff7eedd),
+    body: Stack(
+      children: [
+        PageView(
+          controller: pageController,
+          onPageChanged: (index) {
+            setState(() {
+              isLastPage = (index == 2);
+              buttonText = isLastPage ? 'Get Started' : 'Next';
+            });
+          },
+          children: const [
+            Screen1(),
+            Screen2(),
+            Screen3(),
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 0),
+              child: SmoothPageIndicator(
+                effect: const JumpingDotEffect(
+                  activeDotColor: Color(0xffF69461),
+                  dotColor: Color(0xff3652AD),
                 ),
-              ],
+                controller: pageController,
+                count: 3,
+                onDotClicked: (index) {
+                  pageController.animateToPage(
+                    index,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                  );
+                },
+              ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
+            Padding(
+              padding: const EdgeInsets.only(bottom: 60.0,left: 30,right: 30), // Adjust this padding as necessary
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  !isLastPage
+                      ? TextButton(
+                          onPressed: () {
+                            pageController.jumpToPage(2);
+                            // Add your skip logic here
+                          },
+                          child: Text(
+                            'Skip',
+                            style: GoogleFonts.openSans(
+                              color: const Color(0xff3652AD),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                          ),
+                        )
+                      : const SizedBox(width: 60,),
+                  TextButton(
+                    onPressed: () {
+                      if (pageController.page == 2) {
+                        // Handle "Get Started" logic here
+                      } else {
+                        pageController.nextPage(
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut,
+                        );
+                      }
+                    },
+                    style: TextButton.styleFrom(
+                      minimumSize: const Size(100, 40), // Set a minimum width and height
+                    ),
+                    child: Text(
+                      buttonText,
+                      style: GoogleFonts.openSans(
+                        color: const Color(0xff3652AD),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
 }
